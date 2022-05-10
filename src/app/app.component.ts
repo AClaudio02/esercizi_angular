6627eeb2a57d99626254730a4c817784a8272912
Claudio Atzori <claudio.atzori02@gmail.com>
2022-05-10 19:03:11 +0200

    Esercizio 9.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CounterService } from './services/counter.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app_esercizi';
+
+  constructor(public counterService: CounterService) {
+    this.counterService.showCounter();
+    this.counterService.add(1);
+    this.counterService.sub(2);
+  }
 }
